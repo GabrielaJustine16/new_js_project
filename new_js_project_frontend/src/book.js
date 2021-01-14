b386d79ccc1181ad21ffca6a8ec5b26fb2c86aa2
Gabriela Johnson <johnsongabriela12@gmail.com>
2021-01-13 22:09:03 -0800

    creates user js,book class and constructor alng with css

---
+++ new_js_project_frontend/src/book.js
@@ -0,0 +1,11 @@
+//Params associated with a book
+class Book {
+    constructor(book){
+        this.id = book.id;
+        this.title = book.title;
+        this.author = book.author;
+        this.review = book.review;
+        this.rating = book.rating;
+    }
+
+    
--- new_js_project_frontend/src/user.js
+++ new_js_project_frontend/src/user.js
@@ -1,25 +1,26 @@
-const { userInfo } = require("os")
 
-document.addEventListener('DOMContentLoaded',() => )
+document.addEventListener('DOMContentLoaded', () => {
 
-    userInfo.createUser();
+    User.createUser();
 })
 
-class User{
+//Params associated with a user
+class User {
     constructor(user) {
         this.id = user.id;
         this.name = user.name;
         this.books = user.books;
+     
     }
-
+//POST fetch for creating a User
     static createUser(user) {
         let newUserForm = document.getElementById('new-user-form')
-        newUserForm.addEventListener('submit',function(e){
+        newUserForm.addEventListener('submit', function(e){
             e.preventDefault();
-            fetch('http://localhost:3000/api/v1/users',{
-                method 'POST' ,
-                headers:{
-                    'Content-Type' : 'application/json' ,
+            fetch('http://localhost:3000/users', {
+                method: 'POST',
+                headers: {
+                    'Content-Type' : 'application/json',
                     'Accept' : 'application/json'
                 },
                 body: JSON.stringify({
@@ -28,39 +29,42 @@ class User{
                     }
                 })
             })
-
-            .then(res => {
-                if (!res.ok){
-                    throw new Error();
-                }
-                return res.json();
+                .then(res => {
+                    if (!res.ok) {
+                        throw new Error(); // Will take you to the `catch` below
+                    }
+                    return res.json();
+                })
+                .then (user => {
+                    let newUser = new User(user)
+                    console.log(user)
+                    newUser.displayUser();
+                })
+                .catch(error => {
+                    console.error('User class Error', error)
+                })
             })
-
-            .then (user => {
-                let newUser = new User(user)
-                console.log(user)
-                newUser.displayUser();
-            })
-            .catch(error => {
-                console.error('User class Error', error)
-            })
-        })
     }
 
     displayUser() {
         let body = document.getElementById('container');
         body.innerHTML = ''
         let div = document.createElement('div');
-        div.setAttribute('class','user-greeting');
+        div.setAttribute('class', 'user-greeting');
         let bc = document.getElementById('books-container')
-        bc.classList.remove('hidden')
         let Greeting = document.createElement('p');
-        Greeting.innerHTML = `<h1>Welcome ${this.name}! Tell us what you read this week!</h1>`
+        Greeting.innerHTML = `<h1>Hey ${this.name}! What did you read this week?</h1>`
         div.appendChild(Greeting);
         body.appendChild(div);
+        this.books.forEach(function(book){
+            let newBook = new Book(book)
+            newBook.appendBook()
+        })
+    
 
-
-
-
+        Book.newBookForm(this.id)
     }
+
 }
+        
+
