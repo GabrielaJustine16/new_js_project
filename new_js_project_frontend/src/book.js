0838bd0dc69101afc0ec2765882dcfaad2ebf79f
Gabriela Johnson <johnsongabriela12@gmail.com>
2021-01-13 22:10:57 -0800

    create static function for book class that crabs container and the book form

--- new_js_project_frontend/src/book.js
+++ new_js_project_frontend/src/book.js
@@ -8,4 +8,18 @@ class Book {
         this.rating = book.rating;
     }
 
-    
+    static newBookForm(user_id) {
+        let body = document.getElementById('container');
+        let form = 
+        ` <form id="new-book-form">
+         <label>Book Title:</label>
+         <input type="text" id="book-title"  placeholder: "Title">
+         <label>Author:</label>
+         <input type="text" id="book-author">
+         <label>Review:</label>
+         <input type="text" id="book-review">
+         <label>Rate this book 1-5:</label>
+         <input type="text" id="book-rating">
+         <input type="submit"/>
+         </form>
+         `
